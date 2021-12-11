9d2773acc1868902ee2a279571328b142a5d58a2
chocolat0w0 <2049423+chocolat0w0@users.noreply.github.com>
2021-12-11 23:35:26 +0900

    fix lint error

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 const App: FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Hello, World!
-      </p>
+  <div className="app">
+    <header className="app-header">
+      <img src={logo} className="app-logo" alt="logo" />
+      <p>Hello, World!</p>
       <a
-        className="App-link"
+        className="app-link"
         href="https://reactjs.org"
         target="_blank"
         rel="noopener noreferrer"
--- src/Test.tsx
+++ src/Test.tsx
@@ -4,10 +4,11 @@ import Greets from 'components/Greets';
 const Test: FC = () => (
   <div>
     <Greets name="Patty" times={4}>
-      <span role="img" aria-label="rabbit">��</span>
+      <span role="img" aria-label="rabbit">
+        ��
+      </span>
     </Greets>
   </div>
-
 );
 
 export default Test;
